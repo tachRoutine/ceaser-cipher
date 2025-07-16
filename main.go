4d2a42d55aa9b61aab1c745018d6a194dceac925
tacherasasi <tacherasasi@gmail.com>
2025-07-16 17:18:16 +0300

    Refactor main function to improve code structure and add content flag for encryption

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+)
 
 func main(){
 	numberOfSubs := flag.Int("sub",1,"number of substitutions of the Caesar cipher")
+	content:= flag.String("content","Hello World!","content to be encrypted")
+	fmt.Println("Number of substitutions:", *numberOfSubs)
+	fmt.Println("Content to be encrypted:", *content)
 	flag.Parse()
 }
